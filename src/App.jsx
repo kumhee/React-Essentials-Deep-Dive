fe3074b9d6e0e47e121b768e3e65cdb9f63e25f5
kumhee <kumheee95@gmail.com>
2024-07-18 19:00:57 +0900

    Fix: fix game reload code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
+import GameOver from './components/GameOver.jsx';
 
 const initialGameBoard = [
   [null, null, null],
@@ -14,6 +15,7 @@ const initialGameBoard = [
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
+  //Note: 만약 가장 최근 턴의 플레이어가 'X'라면, 다음 플레이어는 'O'
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
     currentPlayer = 'O';
   }
@@ -27,7 +29,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -36,7 +38,7 @@ function App() {
     gameBoard[row][col] = player;
   }
 
-  //NOTE: 우승자가 있는지 여부 확인
+  //NOTE: 우승자 확인 로직
   let winner;
 
   for (const combination of WINNING_COMBINATIONS) {
@@ -48,6 +50,9 @@ function App() {
       winner = firstSquareSymbol;
     }
   }
+
+  //NOTE: 무승부 상황 처리 로직
+  const hasDraw = gameTurns.length === 9 && !winner; //9번의 게임 중 winner가 없을경우
 
   function handleSelectSquare(rowIndex, colIndex) {
     // NOTE: (curActivePlayer -> 현재 진행중인 player) // 게임 보드에서 어떤 버튼이 선택될 때 호출됨. 현재 활성 플레이어를 'X'에서 'O'로, 또는 'O'에서 'X'로 토글
@@ -66,6 +71,11 @@ function App() {
     });
   }
 
+  //NOTE: Rematch
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -73,6 +83,7 @@ function App() {
           <Player initialName="player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
